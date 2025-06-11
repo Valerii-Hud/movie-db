dbe7107d0c262e7f0ace95ce3fdda2dc0497eab3
Valerii Hud <support@valerii-hud.com>
2025-06-11 19:26:33 +0200

    feat: Homework 11.06.2025

---
+++ js/constants.js
@@ -0,0 +1,18 @@
+export const movieDB = {
+  movies: [
+    'Логан',
+    'Лига справедливости',
+    'Ла-ла лэнд',
+    'Одержимость',
+    'Скотт Пилигрим против...',
+  ],
+};
+
+export const adv = document.querySelectorAll('.promo__adv  img');
+
+export const poster = document.querySelector('.promo__bg'),
+  posterBackgroundImage = 'url("img/bg.jpg")';
+
+export const genre = poster.querySelector('.promo__genre');
+
+export const movieList = document.querySelector('.promo__interactive-list');
